cf9604a8f1771049eb2770483103ce76ad3ddcf7
Sho Sugahara <showgm123@gmail.com>
2019-03-13 18:31:21 +0900

    first

---
+++ app/Http/Controllers/Admin/ProfileController.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Profile;
+use App\Profilehistory;
+use Carbon\Carbon;
+
+class ProfileController extends Controller
+{
+  public function add()
+  {
+    return view('admin.profile.create');
+  }
+
+  public function create(Request $request)
+  {
+    $this->validate($request,Profile::$rules);
+    $profile = Profile::find($request->id);
+    $profile = new Profile;
+    $form = $request->all();
+
+    unset($form['_token']);
+
+    $profile->fill($form);
+    $profile->save();
+
+    return redirect('admin/profile/create');
+  }
+
+ public function edit(Request $request)
+ {
+    $profile = Profile::find($request->id);
+    if (empty($profile)) {
+      abort(404);
+    }
+    return view('admin.profile.edit', ['profile_form' => $profile]);
+ }
+
+ public function update(Request $request)
+ {
+    $this->validate($request, Profile::$rules);
+    $profile = Profile::find($request->id);
+    $profile_form = $request->all();
+
+    unset($profile_form['_token']);
+    $profile->fill($profile_form)->save();
+
+    $profilehistory = new Profilehistory;
+    $profilehistory->profile_id = $profile->id;
+    $profilehistory->edited_at = Carbon::now();
+    $profilehistory->save();
+    
+    return redirect('admin/profile');
+ }
+
+ public function index(Request $request)
+ {
+
+   $cond_title = $request->cond_title;
+   if ($cond_title != '') {
+     $posts = Profile::where('title', $cond_title)->get();
+   } else {
+
+     $posts = Profile::all();
+   }
+   return view('admin.profile.index', ['posts' => $posts, 'cond_title' => $cond_title]);
+ }
+
+ public function delete(Request $request)
+ {
+   $profile = Profile::find($request->id);
+   $profile->delete();
+   return redirect('admin/profile/');
+ }
+}
